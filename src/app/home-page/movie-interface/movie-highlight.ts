df366191289577bb5f3fc9aa40baaa9e6766889d
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-06-16 18:03:30 -0400

    algumas alterações para funcionar o scroll lateral dos card

---
+++ src/app/home-page/movie-interface/movie-highlight.ts
@@ -0,0 +1,9 @@
+export interface MovieHighlight {
+  description: string;
+  gender: string;
+  id: string;
+  imgUrl: string;
+  imgUrlBackground: string;
+  movieRating: number;
+  title: string;
+}
---
+++ src/app/home-page/movie-interface/movie-trending.ts
@@ -0,0 +1,6 @@
+export interface MovieTrending {
+  id: string;
+  imgUrl: string;
+  movieRating: number;
+  title: string;
+}
